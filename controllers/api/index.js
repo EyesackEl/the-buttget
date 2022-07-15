3d0ef2cca85f81aa6b9bc210f1eabfa79b0d2992
Matthew Davis <matthewdavis1372@yahoo.com>
2022-07-14 21:46:13 -0400

    fixed api/index.js file to make it a router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,40 +1,10 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const updateRoutes = require('./updateRoutes');
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+router.use('/login', userRoutes);
+router.use('/signup', userRoutes);
+router.use('/add', updateRoutes)
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({});
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+module.exports = router;
