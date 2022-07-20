52738444a97c6fe6c6cfa2f303f5149ac04524a7
Matthew Davis <matthewdavis1372@yahoo.com>
2022-07-20 00:23:10 -0400

    ":wq"

--- public/js/signup.js
+++ public/js/signup.js
@@ -25,11 +25,5 @@ const signupFormHandler = async () => {
     }
 };
 
-
-
-captcha.generate(); // Draws the image to the canvas
-
-var captchaText = captcha.text();
-
 document
   .querySelector('#signupBtn').addEventListener('click', signupFormHandler);
