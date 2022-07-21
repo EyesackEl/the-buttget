9f424875943ba497a18e5da8ad79b01048fa247f
Matthew Davis <matthewdavis1372@yahoo.com>
2022-07-21 05:21:11 -0400

    cleaning up js files in public folder

--- controllers/api/subcategoryRoutes.js
+++ controllers/api/subcategoryRoutes.js
@@ -35,7 +35,7 @@ router.delete('/:id', withAuth, async (req, res) => {
 
     res.status(200).json(projectData)
   } catch (err) {
-    res.status(500).json(err);
+    res.status(401).json(err);
   }
 });
 
